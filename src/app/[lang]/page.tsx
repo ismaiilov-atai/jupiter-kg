f063201e4b37abe9c16aaf88b4e9020084ce9242
ismaiilov-atai <ataismaiilov@gmail.com>
2024-07-26 11:38:35 -0400

    setup: setup logic navigating to onboarding, added actions (skipOnboarding, setBoardingRequiredCookie, getBoardingRequiredCookie), the data will be stored to cookies, updated README

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,11 +1,19 @@
 import { useTranslations } from 'next-intl';
+import { use } from 'react';
 
+import { getBoardingRequiredCookie } from '@/lib/actions';
 import { redirect } from '@/navigation';
 
 export default function Home() {
 	const t = useTranslations('Landing');
-	// if user is admin redirect it to admin page
-	// redirect('/admin');
+	/*
+		if user is admin redirect it to admin page
+		redirect('/admin');
+		
+		checks user if here for the first time and redirects accordingly
+		const userFirstTime = use(getBoardingRequiredCookie());
+		if (userFirstTime) redirect('/onboarding'); 
+	*/
 
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
